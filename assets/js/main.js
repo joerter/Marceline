111cb50db34bac41539193cd5152951e0ed18bc1
John Oerter <joerter@gmail.com>
2016-03-20 23:07:25 -0500

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,28 +12,3 @@ $(window).scroll(function() {
 		});
     }
 });
-
-$(function() {
-	var backgroundheight = 8000;
-	var now = new Date();
-	var hour = now.getHours();
-	var minute = now.getMinutes();
-	var hourpercent = hour / 24 * 100;
-	var minutepercent = minute / 60 / 24 * 100;
-	var percentofday = Math.round(hourpercent + minutepercent);
-
-	var offset = backgroundheight / 100 * percentofday;
-
-	var offset = offset - (backgroundheight / 4);
-
-	function scrollbackground() {
-   		offset = (offset < 1) ? offset + (backgroundheight - 1) : offset - 1;
-   		$('.masthead').css("background-position", "50% " + offset + "px");
-   		setTimeout(function() {
-			scrollbackground();
-			}, 50
-		);
-   	}
-	scrollbackground();
-});
-
